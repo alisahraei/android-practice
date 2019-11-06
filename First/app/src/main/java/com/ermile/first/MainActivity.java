15098bc3244f2db9b849b4d5b14ab249f1256064
Ali Sahraei <ali.sahraei110@gmail.com>
2019-11-06 16:27:40 +0330

    add textview and button

--- First/app/src/main/java/com/ermile/first/MainActivity.java
+++ First/app/src/main/java/com/ermile/first/MainActivity.java
@@ -3,12 +3,15 @@ package com.ermile.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView ali;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ali = findViewById(R.id.textView);
+        ali.setText("alisahraei");
     }
 }
