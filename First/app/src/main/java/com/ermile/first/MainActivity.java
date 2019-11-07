e6d2c15ed019256fec810f9e9d9a5b26ff61c879
Ali Sahraei <ali.sahraei110@gmail.com>
2019-11-07 16:30:27 +0330

    test 2

--- First/app/src/main/java/com/ermile/first/MainActivity.java
+++ First/app/src/main/java/com/ermile/first/MainActivity.java
@@ -3,15 +3,30 @@ package com.ermile.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     TextView ali;
+    TextView date;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ali = findViewById(R.id.textView);
-        ali.setText("alisahraei");
+        //ali.setText("alisahraei");
+        date = findViewById(R.id.date);
+        date.setText(new Date().toString());
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
