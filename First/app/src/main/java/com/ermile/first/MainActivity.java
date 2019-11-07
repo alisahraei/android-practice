55cf317d635d9e6defa0f22b02bea5ed9324726f
Ali Sahraei <ali.sahraei110@gmail.com>
2019-11-07 18:16:58 +0330

    test-003

--- First/app/src/main/java/com/ermile/first/MainActivity.java
+++ First/app/src/main/java/com/ermile/first/MainActivity.java
@@ -2,17 +2,20 @@ package com.ermile.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextView ali;
     TextView date;
     Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         date = findViewById(R.id.date);
         date.setText(new Date().toString());
         button = findViewById(R.id.button);
+        date.setVisibility(View.INVISIBLE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (v.getId() == R.id.button){
+                    Random random = new Random();
+                    date.setTextColor(Color.rgb(random.nextInt(256),random.nextInt(256),random.nextInt(256)));
+                }
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                date.setVisibility(View.VISIBLE);
+                return false;
             }
         });
     }
+
 }
