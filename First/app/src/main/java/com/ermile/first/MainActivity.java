2edba2d7f252804bafa7dfc65a510078b70eb8b0
Ali Sahraei <ali.sahraei110@gmail.com>
2019-11-08 16:31:02 +0330

    test-005

--- First/app/src/main/java/com/ermile/first/MainActivity.java
+++ First/app/src/main/java/com/ermile/first/MainActivity.java
@@ -2,11 +2,13 @@ package com.ermile.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Date;
 import java.util.Random;
@@ -23,9 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ali = findViewById(R.id.textView);
         //ali.setText("alisahraei");
         date = findViewById(R.id.date);
-        date.setText(new Date().toString());
         button = findViewById(R.id.button);
-        date.setVisibility(View.INVISIBLE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,14 +41,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                }
 //            }
 //        });
+
+
+
+
+
+//        button.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                date.setVisibility(View.VISIBLE);
+//                return true;
+////                when return is false means onLongClick with onClick but return is ture means onLongClick without onClick
+//            }
+//        });
+
+
+
         button.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                date.setVisibility(View.VISIBLE);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
                 return true;
-//                when return is true means onLongClick with onClick but return is false means onLongClick without onClick
             }
         });
+        Toast.makeText(this, "MainActivity: onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "MainActivity: onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        Toast.makeText(this, "MainActivity: onRestart", Toast.LENGTH_SHORT).show();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "MainActivity: onResume", Toast.LENGTH_SHORT).show();
+        date.setText(new Date().toString());
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "MainActivity: onDestroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "MainActivity: onStop", Toast.LENGTH_SHORT).show();
+        super.onStop();
     }
 
     private void randomTextColor() {
@@ -58,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
         randomTextColor();
     }
 }
--- First/app/src/main/java/com/ermile/first/SecondActivity.java
+++ First/app/src/main/java/com/ermile/first/SecondActivity.java
@@ -3,6 +3,7 @@ package com.ermile.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Toast.makeText(this, "SecondActivity: onCreate", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "SecondActivity: onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        Toast.makeText(this, "SecondActivity: onRestart", Toast.LENGTH_SHORT).show();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "SecondActivity: onResume", Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "SecondActivity: onDestroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "SecondActivity: onStop", Toast.LENGTH_SHORT).show();
+        super.onStop();
     }
 }
