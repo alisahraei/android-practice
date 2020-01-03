56c9fd973f6de84b26d4ca84953150ce7370f0cf
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-03 22:58:59 +0330

    spinner design test-026

--- First/app/src/main/java/com/ermile/first/SpinnerActivity.java
+++ First/app/src/main/java/com/ermile/first/SpinnerActivity.java
@@ -3,12 +3,16 @@ package com.ermile.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class SpinnerActivity extends AppCompatActivity {
-
+    Spinner simpleSpinner;
+    String[] simpleItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+        simpleItems = getResources().getStringArray(R.array.tours);
+        simpleSpinner= findViewById(R.id.spinner);
     }
 }
