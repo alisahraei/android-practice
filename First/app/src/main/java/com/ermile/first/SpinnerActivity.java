120f9ec91e31ae505d02fe3e68f06cdaf9fb5ecd
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-04 01:21:14 +0330

    fix fun

--- First/app/src/main/java/com/ermile/first/SpinnerActivity.java
+++ First/app/src/main/java/com/ermile/first/SpinnerActivity.java
@@ -3,7 +3,11 @@ package com.ermile.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SpinnerActivity extends AppCompatActivity {
     Spinner simpleSpinner;
@@ -12,7 +16,21 @@ public class SpinnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+        initSimpleSpinner();
+    }
+
+    private void initSimpleSpinner() {
         simpleItems = getResources().getStringArray(R.array.tours);
         simpleSpinner= findViewById(R.id.spinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, simpleItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        simpleSpinner.setAdapter(adapter);
+        simpleSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SpinnerActivity.this, (String) parent.getSelectedItem(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
