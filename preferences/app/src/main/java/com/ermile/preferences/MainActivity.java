5a1e63cf430be9e221e7163ef05f9a99f1968468
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-04 21:21:34 +0330

    preferences restart

---
+++ preferences/app/src/main/java/com/ermile/preferences/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ermile.preferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
