2505c1f8857cfb0fe769aab64338916066e9fc09
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-04 19:33:24 +0330

    fix spinner

--- First/app/src/main/java/com/ermile/first/CountrySpinnerAdapter.java
+++ First/app/src/main/java/com/ermile/first/CountrySpinnerAdapter.java
@@ -1,6 +1,67 @@
 package com.ermile.first;
-
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class CountrySpinnerAdapter extends BaseAdapter {
+
+    private Context context;
+    private int[] flags;
+    private String[] names;
+    private LayoutInflater inflater;
+    public CountrySpinnerAdapter(Context context, int[] countryFlagIds, String[] countryNames){
+        this.context = context;
+        this.flags = countryFlagIds;
+        this.names = countryNames;
+        inflater = LayoutInflater.from(context);
+    }
+
+
+    @Override
+    public int getCount() {
+        return flags.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return names[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.custom_spnner_item, viewGroup, false);
+            holder = new ViewHolder();
+            holder.country_flag = convertView.findViewById(R.id.country_flag);
+            holder.country_name = convertView.findViewById(R.id.country_name);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.fill(position);
+        return convertView;
+    }
+
+
+    public class ViewHolder{
+        public ImageView country_flag;
+        public TextView country_name;
+
+        public void fill(int position){
+            country_flag.setImageResource(flags[position]);
+            country_name.setText(names[position]);
+        }
+    }
 }
--- First/app/src/main/java/com/ermile/first/SpinnerActivity.java
+++ First/app/src/main/java/com/ermile/first/SpinnerActivity.java
@@ -11,24 +11,58 @@ import android.widget.Toast;
 
 public class SpinnerActivity extends AppCompatActivity {
     Spinner simpleSpinner;
+    Spinner customSpinner;
+    int[] countryFlags = {R.drawable.flag_cn, R.drawable.flag_cz,
+            R.drawable.flag_dk, R.drawable.flag_fr, R.drawable.flag_de,
+            R.drawable.flag_ir, R.drawable.flag_it, R.drawable.flag_jp,
+            R.drawable.flag_kr, R.drawable.flag_pt, R.drawable.flag_pl,
+            R.drawable.flag_es, R.drawable.flag_tr};
+
+    String[] countryNames = { "China", "Czech", "Denmark", "France", "Germany",
+            "Iran", "Italy", "Japan", "Korea", "Portugal", "Poland", "Spain", "Turkey"};
     String[] simpleItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
         initSimpleSpinner();
+        initCustomSpinner();
     }
 
     private void initSimpleSpinner() {
         simpleItems = getResources().getStringArray(R.array.tours);
-        simpleSpinner= findViewById(R.id.spinner);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, simpleItems);
+        simpleSpinner= findViewById(R.id.simple_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, simpleItems);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         simpleSpinner.setAdapter(adapter);
-        simpleSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        simpleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(SpinnerActivity.this, (String) parent.getSelectedItem(), Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(SpinnerActivity.this,
+                        (String) adapterView.getSelectedItem(),
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    private void initCustomSpinner() {
+        customSpinner = (Spinner) findViewById(R.id.custom_spinner);
+        CountrySpinnerAdapter adapter = new CountrySpinnerAdapter(this, countryFlags, countryNames);
+        customSpinner.setAdapter(adapter);
+        customSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(SpinnerActivity.this,
+                        (String) adapterView.getSelectedItem(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
     }
