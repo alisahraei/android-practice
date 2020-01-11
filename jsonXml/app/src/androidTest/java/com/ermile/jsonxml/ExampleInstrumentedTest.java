5d2f9ff2ff1f0c502586373cbcb875dcc4c8b198
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-11 19:13:50 +0330

    json and xml new test

--- jsonXml/app/src/androidTest/java/com/ermile/jsonxml/ExampleInstrumentedTest.java
+++ jsonXml/app/src/androidTest/java/com/ermile/jsonxml/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.ermile.jsonxml;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- jsonXml/app/src/main/java/com/ermile/jsonxml/Flower.java
+++ jsonXml/app/src/main/java/com/ermile/jsonxml/Flower.java
@@ -1,4 +1,34 @@
 package com.ermile.jsonxml;
 
+import androidx.annotation.NonNull;
+
 public class Flower {
+    private long productId;
+    private String category;
+    private String name;
+    private String instruction;
+    private  double price;
+    private String photo;
+
+    public Flower(){}
+
+    public long getProductId() { return productId; }
+    public void setProductId(long productId) { this.productId = productId; }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getInstruction() { return instruction; }
+    public void setInstruction(String instruction) { this.instruction = instruction; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public String getPhoto() { return photo; }
+    public void setPhoto(String photo) { this.photo = photo; }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+    }
 }
