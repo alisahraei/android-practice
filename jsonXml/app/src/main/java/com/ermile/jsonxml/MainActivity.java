23e11c12ef4fa0db63fd45fd3900537ad9c26329
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-11 19:58:40 +0330

    test-031

--- jsonXml/app/src/main/java/com/ermile/jsonxml/MainActivity.java
+++ jsonXml/app/src/main/java/com/ermile/jsonxml/MainActivity.java
@@ -3,12 +3,45 @@ package com.ermile.jsonxml;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button btn_xml , btn_json;
+     ListView listview;
+     private List<Flower> flowers;
+     private ArrayAdapter<Flower> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_xml = findViewById(R.id.btn_xml);
+        btn_json = findViewById(R.id.btn_json);
+        listview = findViewById(R.id.listview);
+
+
+        btn_xml.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flowers = new FlowerXmlPullParser(MainActivity.this).parseXml();
+                Toast.makeText(MainActivity.this, "Returned" + flowers.size() + "items", Toast.LENGTH_SHORT).show();
+                refreshDisplay();
+            }
+        });
+    }
+    public void refreshDisplay(){
+        if (flowers == null){
+            flowers = new ArrayList<>();
+
+        }
+        adapter = new ArrayAdapter<Flower>(this, android.R.layout.simple_list_item_1, flowers);
+        listview.setAdapter(adapter);
     }
 }
