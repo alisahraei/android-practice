5b9920c30c2f8e96d964945f42202c03f3554cca
Ali Sahraei <ali.sahraei110@gmail.com>
2020-01-18 18:19:22 +0330

    new activity and set method

--- IntroSliderActivity/app/src/main/java/com/ermile/introslideractivity/MainActivity.java
+++ IntroSliderActivity/app/src/main/java/com/ermile/introslideractivity/MainActivity.java
@@ -2,6 +2,7 @@ package com.ermile.introslideractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SliderPrefManager prefman = new SliderPrefManager(this);
+        if (prefman.startSlider()){
+            Intent intent = new Intent(this, IntroSliderActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
