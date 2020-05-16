242d122d97ec158538a66b95d2e8ab8635496874
Ali Sahraei <ali.sahraei110@gmail.com>
2020-05-16 08:07:10 +0430

    test for finder

--- NSD_discover/app/src/main/java/com/ermile/nsd_discover/MainActivity.java
+++ NSD_discover/app/src/main/java/com/ermile/nsd_discover/MainActivity.java
@@ -2,13 +2,106 @@ package com.ermile.nsd_discover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Method;
+import java.nio.channels.Channel;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final IntentFilter intentFilter = new IntentFilter();
+    Channel channel;
+    WifiP2pManager manager;
+    Button btn_WIFi, btn_test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        btn_WIFi = findViewById(R.id.btn_turnOn);
+        btn_test = findViewById(R.id.btn_test);
+        btn_WIFi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TurnOnWifi();
+            }
+        });
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onReceive();
+            }
+        });
     }
+
+    private void TurnOnWifi() {
+        manager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        channel = (Channel) manager.initialize(getApplicationContext(),
+                getMainLooper(), null);
+
+        try {
+            Class<?> wifiManager = Class
+                    .forName("android.net.wifi.p2p.WifiP2pManager");
+
+            Method method = wifiManager
+                    .getMethod(
+                            "enableP2p",
+                            WifiP2pManager.Channel.class);
+
+            method.invoke(manager, channel);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    activity.setIsWifiP2pEnabled(true);
+            } else {
+                activity.setIsWifiP2pEnabled(false);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // The peer list has changed! We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed! We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+                    .findFragmentById(R.id.frag_list);
+            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+
+        }
+    }
+
+
+
+
 }
